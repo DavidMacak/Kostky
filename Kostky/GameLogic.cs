69cf3e718c542a8aafe914757b3e80092bf56648
David Macák <macak.d@outlook.cz>
2022-01-17 12:32:47 +0100

    Game logic done...i guess?

--- Kostky/GameLogic.cs
+++ Kostky/GameLogic.cs
@@ -13,6 +13,7 @@ namespace Kostky
         public List<bool> canDiceBeThrown = new List<bool>();
         private List<int> howManyNumsThrown = new List<int>();
         private int roundsLeft = 0;
+        private int roundScore = 0;
 
         public GameLogic()
         {
@@ -24,25 +25,56 @@ namespace Kostky
         public void StartRound(Player player, int maxRounds)
         {
             roundsLeft = maxRounds;
+            roundScore = 0;
+
             Console.WriteLine();
             BetterText.GreenText($"Právě hraje {player.Name} se skórem {player.Score} bodů.");
 
             while(roundsLeft > 0)
             {
-                //Console.WriteLine("Zbývá kol: " + roundsLeft);
+                roundsLeft--;
                 dices.ThrowAllDices();
                 dices.ShowDiceValues();
-                CheckDices(player);
+                CheckDices();
+                
+            }
+
+            if (roundScore >= 350 && roundsLeft == 0)
+            {
+                player.Score += roundScore;
             }
 
         }
 
-        public void CheckDices(Player player)
+        private void IgnoreOnesFives(bool ones, bool fives, int indexNumber)
         {
-            roundsLeft--;
+            if (indexNumber == 0)
+            {
+                ones = true;
+            }
+            else if (indexNumber == 4)
+            {
+                fives = true;
+            }
+        }
+        private bool IgnoreOnesFives(int indexNumber, int count)
+        {
+            if (indexNumber == 0 || indexNumber ==4 && count == 2)
+            {
+               return true;
+            }
+            return false;
+        }
+
+        public void CheckDices()
+        {
             int triplets = 0;
             int pairs = 0;
             int postupka = 6;
+            bool ones = false;
+            bool fives = false;
+            bool ignoreOnes = false;
+            bool ignoreFives = false;
 
             //List s počtem kolikrát se hodilo nějaké číslo
             for (int i = 0; i<6; i++)
@@ -61,22 +93,25 @@ namespace Kostky
                 if(howManyNumsThrown[i] == 6)
                 {
                     BetterText.MagentaText("6 stejných! +3000");
-                    player.Score += 3000;
+                    roundScore += 3000;
                     roundsLeft = 3;
-                    break;
+                    //IgnoreOnesFives(ignoreOnes, ignoreFives, i);
+                    return;
                 }
 
                 if (howManyNumsThrown[i] == 5)
                 {
                     BetterText.MagentaText("5 stejných! +2000");
-                    player.Score += 2000;
+                    roundScore += 2000;
+                    IgnoreOnesFives(ignoreOnes, ignoreFives, i);
                     break;
                 }
 
                 if (howManyNumsThrown[i] == 4)
                 {
                     BetterText.MagentaText("4 stejné! +1000");
-                    player.Score += 1000;
+                    roundScore += 1000;
+                    IgnoreOnesFives(ignoreOnes, ignoreFives, i);
                     break;
                 }
 
@@ -86,19 +121,27 @@ namespace Kostky
                     if(i == 0)
                     {
                         BetterText.MagentaText("3 stejné jedničky! +1000");
-                        player.Score += 1000;
+                        ignoreOnes = true;
+                        roundScore += 1000;
+                        ones = true;
                     }
                     else
                     {
                         int score = (i + 1) * 100;
                         BetterText.MagentaText("3 stejné! +" + score);
-                        player.Score += score;
+                        if(i == 4)
+                        {
+                            fives = true;
+                            ignoreFives = true;
+                        }
+                        roundScore += score;
                     }
 
-                    if(triplets == 2)
+                    if (triplets == 2)
                     {
-
-                        break;
+                        BetterText.MagentaText("Dvě trojice!");
+                        roundsLeft = 3;
+                        return;
 
                     }
                 }
@@ -110,8 +153,14 @@ namespace Kostky
                     if (pairs == 3)
                     {
                         BetterText.MagentaText("Dvojičky! +1500");
-                        player.Score += 1500;
-                        break;
+                        roundScore += 1500;
+                        roundsLeft = 3;
+                        //for (int j = 0; j < 6; j++)
+                        //{
+                        //    ignoreOnes = IgnoreOnesFives(j, howManyNumsThrown[j]);
+                        //    ignoreFives = IgnoreOnesFives(j, howManyNumsThrown[j]);
+                        //}
+                        return;
 
                     }
                 }
@@ -123,25 +172,35 @@ namespace Kostky
                     if(postupka == 0)
                     {
                         BetterText.MagentaText("Postupka! +2500");
-                        player.Score += 2500;
-                        break;
+                        roundScore += 2500;
+                        roundsLeft = 3;
+                        ignoreOnes = true;
+                        ignoreFives = true;
+                        return;
                     }
 
                 }
                 
             }
 
-            if (howManyNumsThrown[0] > 0 && howManyNumsThrown[0] <= 2)
+            if (howManyNumsThrown[0] > 0 && howManyNumsThrown[0] <= 2 && !ignoreOnes)
             {
+                ones = true;
                 int score = howManyNumsThrown[0] * 100;
                 BetterText.MagentaText("Jednička! +" + score);
-                player.Score += score;
+                roundScore += score;
             }
-            else if (howManyNumsThrown[4] > 0 && howManyNumsThrown[4] <= 2)
+            if (howManyNumsThrown[4] > 0 && howManyNumsThrown[4] <= 2 && !ignoreFives)
             {
+                fives = true;
                 int score = howManyNumsThrown[4] * 50;
                 BetterText.MagentaText("Pětka! +" + score);
-                player.Score += score;
+                roundScore += score;
+            }
+
+            if(!ones && !fives)
+            {
+                roundsLeft = 0;
             }
 
         }        
